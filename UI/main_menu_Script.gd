400fd97cca71bf8e0b87a5cacd70759d0dd8dfa2
Joannerigt <Joannerigt@gmail.com>
2025-10-03 19:59:03 +0200

    MainMenu+PortalSprite

---
+++ UI/main_menu_Script.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+func _on_level_select_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+ 
